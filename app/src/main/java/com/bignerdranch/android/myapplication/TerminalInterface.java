b94b37644d1744c7d536d8d8038493c3444714b3
권철현 <69569812+kkcchh9999@users.noreply.github.com>
2021-11-26 17:37:17 +0900

    Update and rename Terminal.java to TerminalInterface.java

--- app/src/main/java/com/bignerdranch/android/myapplication/TerminalInterface.java
+++ app/src/main/java/com/bignerdranch/android/myapplication/TerminalInterface.java
@@ -12,8 +12,11 @@ import retrofit2.http.Path;
 //http://api.nubija.com:1577/ ubike/nubijaInfoApi.do?apikey=kTKnDZYpryizkfmPsCyu
 //레트로핏 인터페이스
 
-public interface Terminal {
+public interface TerminalInterface {
     @GET("http://api.nubija.com:1577/ubike/nubijaInfoApi.do?apikey=kTKnDZYpryizkfmPsCyu")
     Call<TerminalInfo> getPosts(@Path("post") String post);
 
 }
+
+
+//해당클래스 이름 바꾸셈!! 충돌난다 
